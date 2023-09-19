2f035765ef9322f018c2444ce6f99478e1f93532
FARIZIMAULIDAN <farizimaulidan@gmail.com>
2023-09-19 14:50:02 +0700

    Program1

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/',(req,res)=>{
+    res.send('halo dedek')
+})
+
+app.listen(port,()=>{
+    console.log(`aplikasi berjalan di http:://localhost:${port}`)
+})
